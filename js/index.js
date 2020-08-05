7130dc624564c99f906cf4cdd00ad5f73638f866
BrettMcAdams96@gmail.com <brettmcadams96@gmail.com>
2020-08-04 20:27:34 -0400

    finished assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+// const myHead = document.querySelector(".funBus");
+// const myBus = document.querySelector(".bus");
+// const myNav = document.querySelector(".funBus");
+// const myDest = document.querySelector(".funBus");
+// const myLogo = document.querySelector(".funBus");
+// const myHead = document.querySelector(".funBus");
+// const myHead = document.querySelector(".funBus");
+// const myHead = document.querySelector(".funBus");
+// const myHead = document.querySelector(".funBus");
+// const myHead = document.querySelector(".funBus");
+
+document.querySelector(".funBus").addEventListener("click", () => {
+    style.color = "blue";
+});
